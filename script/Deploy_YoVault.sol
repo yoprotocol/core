87489e60d63bb2fc21d4d75c1c6154f470290d53
Krasimir Raykov <raykov@weichain.tech>
2025-09-17 21:04:23 +0300

    feat: enhance Deploy_YoVault script with deposit and pause functionality

--- script/Deploy_YoVault.sol
+++ script/Deploy_YoVault.sol
@@ -16,7 +16,9 @@ contract Deploy is BaseScript {
         address _asset,
         address _owner,
         address _authority,
-        address _vault
+        address _vault,
+        uint256 _depositAmount,
+        bool _pause
     )
         public
         broadcast
@@ -46,47 +48,41 @@ contract Deploy is BaseScript {
         if (_vault == address(0)) {
             impl = new YoVault();
             console.log("yoVault implementation deployed at: ", address(impl));
-            return (impl, authority);
         } else {
             impl = YoVault(payable(_vault));
-            console.log("yoVault implementation deployed at: ", address(impl));
-
-            bytes memory data =
-                abi.encodeWithSelector(vault.initialize.selector, IERC20(_asset), _owner, _name, _symbol);
-
-            TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(address(impl), _owner, data);
-
-            vault = YoVault(payable(address(proxy)));
-            console.log("yoVault proxy deployed at: ", address(vault));
-
-            vault.setAuthority(authority);
-            console.log("Authority set for vault");
-
-            vault.pause();
-
-            console.log("\nVault data:");
-            console.log("Underlying asset:", address(vault.asset()));
-            console.log("Name:", vault.name());
-            console.log("Symbol:", vault.symbol());
-            console.log("Owner:", vault.owner());
-            console.log("Total Supply:", vault.totalSupply());
-            console.log("Paused:", vault.paused());
-            console.log("Authority:", address(vault.authority()));
-            console.log("Deposit Fee:", vault.feeOnDeposit());
-            console.log("Withdraw Fee:", vault.feeOnDeposit());
-            console.log("FeeRecipient:", vault.feeRecipient());
         }
+        console.log("yoVault implementation deployed at: ", address(impl));
+
+        bytes memory data = abi.encodeWithSelector(vault.initialize.selector, IERC20(_asset), _owner, _name, _symbol);
+
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(address(impl), _owner, data);
+
+        vault = YoVault(payable(address(proxy)));
+        console.log("yoVault proxy deployed at: ", address(vault));
+
+        vault.setAuthority(authority);
+        console.log("Authority set for vault");
+
+        if (_depositAmount > 0) {
+            IERC20(_asset).approve(address(vault), _depositAmount);
+            vault.deposit(_depositAmount, _owner);
+            console.log("Deposited: ", _depositAmount);
+        }
+
+        if (_pause) {
+            vault.pause();
+        }
+
+        console.log("\nVault data:");
+        console.log("Underlying asset:", address(vault.asset()));
+        console.log("Name:", vault.name());
+        console.log("Symbol:", vault.symbol());
+        console.log("Owner:", vault.owner());
+        console.log("Total Supply:", vault.totalSupply());
+        console.log("Paused:", vault.paused());
+        console.log("Authority:", address(vault.authority()));
+        console.log("Deposit Fee:", vault.feeOnDeposit());
+        console.log("Withdraw Fee:", vault.feeOnDeposit());
+        console.log("FeeRecipient:", vault.feeRecipient());
     }
 }
-
-// forge script script/Deploy_InvestmentVault.sol:Deploy --fork-url
-// https://arb-mainnet.g.alchemy.com/v2/B5NOppp7QFB18K4bPkrmmDFMjhgQEOST -vvvv --sig
-// "run(string,string,address,address,address,address)" "yoVaultBTC" "yoBTC" 0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf
-// 0xd9C452E307A9521BAE23cb9E83bA71BE057207AE 0x9524e25079b1b04d904865704783a5aa0202d44d
-// 0x0000000000000000000000000000000000000000
-
-// forge script script/Deploy_InvestmentVault.sol:Deploy --fork-url
-// https://arb-mainnet.g.alchemy.com/v2/B5NOppp7QFB18K4bPkrmmDFMjhgQEOST -vvvv --sig
-// "run(string,string,address,address,address,address)" "yoVaultUSD" "yoUSD" 0xaf88d065e77c8cc2239327c5edb3a432268e5831
-// 0x5641d005b8f541BDFdDBFACDc910f2aD5E0c3C21 0x9524e25079b1b04d904865704783a5aa0202d44d
-// 0x0000000000000000000000000000000000000000
